27fefb9ce4ab45c5db19cdda924b02a633adb9ae
Bryson Mercury <reinardssaulitis@gmail.com>
2023-08-28 10:11:22 +0300

    Remove warmotech

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -19,7 +19,6 @@ export default function Navigation() {
             <Link  className={router.pathname == "/" ? "active" : "navlink"} href="/">Sākums</Link>
             <Link className={router.pathname == "/parmums" ? "active" : "navlink"} href="/parmums">Par Mums</Link>
             <Link className={router.pathname == "/buvnieciba" ? "active" : "navlink"} href="/buvnieciba">Būvniecība</Link>
-            <Link className={router.pathname == "/warmotech" ? "active" : "navlink"} href="/warmotech">Warmotech</Link>
             <Link className="navlink" href="#kontakti">Kontakti</Link>
           </nav>
         </div>
